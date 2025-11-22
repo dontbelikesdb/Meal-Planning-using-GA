fe769e52146b3645337d86424a7c756f4b82cff1
Soumyadeep Biswas <148277514+dontbelikesdb@users.noreply.github.com>
2025-11-22 23:51:28 +0530

    Initialize App component with routing

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Navbar from "./components/Navbar";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
